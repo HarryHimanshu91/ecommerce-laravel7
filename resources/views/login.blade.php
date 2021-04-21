9cceb7e43c4b475c2fae3708e433d0e5b13e6536
himanshu.chand <himanshu.chand@kelltontech.com>
2021-04-21 19:36:35 +0530

    custom login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,14 +3,15 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-md-4 col-sm-offset-4">
-            <form action="/action_page.php">
+            <form action="{{ route('login') }}" method="post">
+              @csrf 
                 <div class="form-group">
                     <label for="email">Email address:</label>
-                    <input type="email" class="form-control" id="email">
+                    <input type="email" class="form-control" name="email" id="email">
                 </div>
                 <div class="form-group">
                     <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd">
+                    <input type="password" class="form-control" name="password" id="pwd">
                 </div>
                
                 <button type="submit" class="btn btn-default">Submit</button>
